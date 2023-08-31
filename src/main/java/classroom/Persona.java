99bd9edd6eff522477de4058867e01ca3a37fa8f
sebasz-a <jpabuena@unal.edu.co>
2023-08-31 11:19:36 -0500

    Prueba 1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,9 +8,13 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
+    public Persona() {
+    	this.nombre = "Sin nombre";
+    	this.cedula = 0;
+    }
+    
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -31,6 +35,7 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
     }
     
